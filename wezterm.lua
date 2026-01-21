f3b09e782bb66eec3f3ce87cc3976bed4e4bcdf0
Eric Wong <wsdjeg@outlook.com>
2026-01-21 21:15:52 +0800

    hide tab bar if only one tab

--- wezterm.lua
+++ wezterm.lua
@@ -11,6 +11,7 @@ config.default_prog = { 'nvim' }
 -- For example, changing the initial geometry for new windows:
 config.initial_cols = 120
 config.initial_rows = 28
+config.hide_tab_bar_if_only_one_tab = true
 
 -- or, changing the font size and color scheme.
 config.font_size = 13
